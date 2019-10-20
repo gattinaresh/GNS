5d6cd467543a75203474fd485eb817c9ee6e3605
naresh <gattinareshbabu@gmail.com>
2019-10-20 20:23:49 +0530

    Test Commit

--- user-services/src/main/java/com/gns/user/services/controller/WelcomeController.java
+++ user-services/src/main/java/com/gns/user/services/controller/WelcomeController.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
+ * Home Page Controller
+ */
 @Controller
 public class WelcomeController {
 
