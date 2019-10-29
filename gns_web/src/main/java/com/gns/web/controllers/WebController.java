e0622361643e3117bf0d440da941229728f1b71c
naresh <gattinareshbabu@gmail.com>
2019-10-29 22:58:53 +0530

    Added Login Page

--- gns_web/src/main/java/com/gns/web/controllers/WebController.java
+++ gns_web/src/main/java/com/gns/web/controllers/WebController.java
@@ -8,21 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WelcomeController {
+public class WebController {
 
-	Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+	Logger logger = LoggerFactory.getLogger(WebController.class);
 
 	@RequestMapping("/")
-	public String comingSoon(Map<String, Object> model) {
-		logger.info("coming-soon :::::::");
+	public String comingSoonPage(Map<String, Object> model) {
 		return "coming-soon";
 	}
 
-	// coming-soon
 	@RequestMapping("/home")
-	public String welcome(Map<String, Object> model) {
-		logger.info("welcome :::::::");
+	public String welcomePage(Map<String, Object> model) {
 		return "welcome";
+	}
+
+	@RequestMapping("/login")
+	public String loginPage(Map<String, Object> model) {
+		return "login";
 	}
 
 }
